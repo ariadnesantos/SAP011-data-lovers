cd7f5ab4ec2f217218661b62e044bb20759c2516
laiscosta25 <laiscosta.c2@gmail.com>
2023-08-22 11:51:20 -0300

    HTML Finalizado

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
-// estas funciones son de ejemplo
+export const dataBB = {
+  function (characters, value, key) {
+    const order = characters.filter(function (character) {
+      const filtered = character[key].includes(value);
 
+      return filtered;
+    });
+  }
+}
 export const example = () => {
   return 'example';
 };
